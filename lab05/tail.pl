40fd97ecc5317018388b9dd15a0fa44fdcc46450
William Fan <william.fan1@student.unsw.edu.au>
2016-08-28 21:13:42 +1000

    options work

--- lab05/tail.pl
+++ lab05/tail.pl
@@ -1,19 +1,28 @@
 #!/usr/bin/perl -w
 @files = ();
+$number = 10;
 foreach $arg (@ARGV) {
     if ($arg eq "--version") {
         print "$0: version 0.1\n";
         exit(0);
     }
-    # handle other options
-    # ...
+    elsif ($arg =~ "-[0-9]") {
+		$number = $arg;
+		$number =~ s/-//;
+	}
     else {
         push @files, $arg;
     }
 }
 foreach $f (@files) {
     open(F,"<$f") or die "$0: can't open $f: $!\n";
-    # process F
-    #...
+	while (<>){
+		while ($number != 0){
+			
+		}
+		print;
+	}
+	print "\n";
     close(F);
 }
+
