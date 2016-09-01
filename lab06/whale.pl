3ba73feefaeedef5b8689a654e5695cf67dfd25e
William Fan <william.fan1@student.unsw.edu.au>
2016-09-01 21:15:08 +1000

    final

--- lab06/whale.pl
+++ lab06/whale.pl
@@ -3,14 +3,14 @@
 $count = 0;
 $pod = 0;
 while ($line = <STDIN>){
-	push @total, $line;
+	push @total, $line; #push input into array
 }
-foreach $arg(@total){
-	chomp($arg);
-	($number, $animal) = split(" ", $arg, 2);
+foreach $arg(@total){ 
+	chomp($arg);	
+	($number, $animal) = split(" ", $arg, 2); #split input into two
 	if (defined $animal && $ARGV[0] eq $animal){
-		$count += $number;
-		$pod++;
+		$count += $number; 	#add to count of individuals
+		$pod++;				#add to count of pod
 	}
 }
 print "$ARGV[0] observations: $pod pods, $count individuals\n";
