b007bb8b85e803a6604b00c480f44db52e5cab16
William Fan <william.fan1@student.unsw.edu.au>
2016-09-01 21:06:20 +1000

    passes autotests

--- lab06/whale.pl
+++ lab06/whale.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
 @total = ();
 $count = 0;
+$pod = 0;
 while ($line = <STDIN>){
 	push @total, $line;
 }
@@ -9,5 +10,7 @@ foreach $arg(@total){
 	($number, $animal) = split(" ", $arg, 2);
 	if (defined $animal && $ARGV[0] eq $animal){
 		$count += $number;
+		$pod++;
 	}
 }
+print "$ARGV[0] observations: $pod pods, $count individuals\n"
