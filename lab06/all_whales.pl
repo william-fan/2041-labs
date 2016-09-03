a529fc44acaa2a59cce00d74ee0c4690b5254a4e
William Fan <william.fan1@student.unsw.edu.au>
2016-09-03 17:36:32 +1000

    passes autotests

--- lab06/all_whales.pl
+++ lab06/all_whales.pl
@@ -1,25 +1,51 @@
 #!/usr/bin/perl -w
 @total = ();
+@edit = ();
 @list = ();
+@sort = ();
 $count = 0;
 $pod = 0;
+$temp = 0;
 while ($line = <STDIN>){
 	push @total, $line; #push input into array
 }
 
 foreach $arg(@total){
-	chomp($arg);		
-	($number, $animal) = split(" ", $arg, 2);  #split input into two
-	push @list, $animal;	#add list to list of species
-}
-foreach $species(@list){
-	foreach $arg(@total){
-		chomp($arg);
-		($number, $animal) = split(" ", $arg, 2);
-		if (defined $animal && $ARGV[0] eq $animal){
-			$count += $number;
-			$pod++;
+	$a = lc($arg);	
+	$temp = 0;	#flag if animal already exists
+	chomp($a);		
+	$a =~ s/\s+$//;
+	$a =~ s/\s+/ /g;		#remove whitespace and trailing s
+	$a =~ s/s$//g;
+	push @edit, $a;
+	#print lc($arg);
+	($number, $animal) = split(" ", $a, 2);  #split input into two
+	#print "$animal|";
+	foreach $type(@list){
+		if ($type eq $animal){	#check if animal exists in list
+			$temp = 1;
 		}
 	}
-	print "$ARGV[0] observations: $pod pods, $count individuals\n";
+	if ($temp == 0) { push @list, $animal }; #add list to list of species
+	
+}
+
+foreach $species(@list){	#loop through species
+	$count = 0;
+	$pod = 0;
+	foreach $arg(@edit){	#loop through edited stdin
+		chomp($arg);
+		($number, $animal) = split(" ", lc($arg), 2);		
+		if (defined $animal && $species eq $animal){
+			$count += $number;		#add to final count
+			$pod++;
+		}
+		#print "$arg|";
+	}
+	push @sort, "$species observations: $pod pods, $count individuals\n";	
+}
+
+@sort = sort @sort;	#sort output
+foreach $arg(@sort){
+	print $arg;
 }
