0e578cc4d7a20145541007700b4158fdda9905a2
William Fan <william.fan1@student.unsw.edu.au>
2016-09-05 22:36:38 +1000

    this is a bad commit message

--- lab06/echon.py
+++ lab06/echon.py
@@ -2,15 +2,15 @@
 import sys
 
 if (len(sys.argv) != 3 ):
-	print "Usage: ./echon.py <number of lines> <string>"
+	print "Usage: ./echon.py <number of lines> <string>"	#check if number of arguments equal to 3
 	exit()
 try:
 	number = int(sys.argv[1])
 except:
-	print "./echon.py: argument 1 must be a non-negative integer"
+	print "./echon.py: argument 1 must be a non-negative integer"	#try changing first argument into int
 	exit()
 try:
-	int(sys.argv[1]) < 0
+	int(sys.argv[1]) < 0			#try checking if argument is less than zero
 except:
 	print "./echon.py: argument 1 must be a non-negative integer"
 	exit()
@@ -19,5 +19,5 @@ number = int(sys.argv[1]);
 string = str(sys.argv[2]);
 
 while (number != 0):
-	print string
+	print string	#print while number is not zero
 	number -= 1
