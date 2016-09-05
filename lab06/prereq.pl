03d818011d8b604c32ca3dd11e512cdf4c730638
William Fan <william.fan1@student.unsw.edu.au>
2016-09-05 19:41:58 +1000

    passes autotests

--- lab06/prereq.pl
+++ lab06/prereq.pl
@@ -1,27 +1,27 @@
 #!/usr/bin/perl -w
 @list = ();
 $count = 0;
-$url = "http://www.handbook.unsw.edu.au/undergraduate/courses/2015/$ARGV[0].html";
+$url = "http://www.handbook.unsw.edu.au/undergraduate/courses/2015/$ARGV[0].html"; 	#open undergraduate
 open F, "wget -q -O- $url|" or die "can't open $url";
 
 
-while ($count != 2){
-	while ($line = <F>) {
-		if ($line =~ "Prerequisite: .*<" || $line =~ "Prerequisites: .*<"){
+while ($count != 2){ 	#loop twice
+	while ($line = <F>) {	
+		if ($line =~ "Prerequisite: .*<" || $line =~ "Prerequisites: .*<"){ #check for lines
 			$temp = $line;
-			$temp =~ s/<\/p>.*//;
-			chomp($temp);
-			push @list,($temp =~ m/[A-Z]{4}[0-9]{4}/g);
+			$temp =~ s/<\/p>.*//;	#remove past </p> tag
+			chomp($temp);	
+			push @list,($temp =~ m/[A-Z]{4}[0-9]{4}/g);	#store matching pattern
 		}
 	}
-	$url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2015/$ARGV[0].html";
+	$url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2015/$ARGV[0].html"; #open postgraduate
 	open F, "wget -q -O- $url|" or die "can't open $url";
 	$count++;
 }
 
-@list = sort @list;
+@list = sort @list; #sort list
 
 foreach $arg (@list){
-	print "$arg\n";
+	print "$arg\n";	
 }
 	
