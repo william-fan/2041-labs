91026e8b7ee840a6b9c6db761323a3a1935635fc
William Fan <william.fan1@student.unsw.edu.au>
2016-09-05 22:38:11 +1000

    passes autotests

--- lab06/echon.py
+++ lab06/echon.py
@@ -4,11 +4,13 @@ import sys
 if (len(sys.argv) != 3 ):
 	print "Usage: ./echon.py <number of lines> <string>"	#check if number of arguments equal to 3
 	exit()
+
 try:
 	number = int(sys.argv[1])
 except:
 	print "./echon.py: argument 1 must be a non-negative integer"	#try changing first argument into int
 	exit()
+
 try:
 	int(sys.argv[1]) < 0			#try checking if argument is less than zero
 except:
