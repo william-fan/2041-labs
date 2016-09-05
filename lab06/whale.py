c447e8eb0596f9717877d6b3d133900f9377dac9
William Fan <william.fan1@student.unsw.edu.au>
2016-09-05 23:54:27 +1000

    not working

---
+++ lab06/whale.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import string
+import sys
+total = []
+count = 0
+pod = 0
+
+for line in sys.stdin:
+	total.append(line) #push input into array
+
+for arg in total:
+	arg.strip
+	number, animal = arg.split(" ", 1) 		#split input into two
+
+	if sys.argv[1] == animal:
+		count += int(number) 	#add to count of individuals
+		pod += 1				#add to count of pod
+
+
+print ("%s observations: %s pods, %s individuals\n" % (sys.argv[1], pod, count))
