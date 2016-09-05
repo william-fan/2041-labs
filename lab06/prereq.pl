b2085f36653e30f593fadfffd394cbfe198681e4
William Fan <william.fan1@student.unsw.edu.au>
2016-09-05 19:23:47 +1000

    web scraping isn't as cool as whales

--- lab06/prereq.pl
+++ lab06/prereq.pl
@@ -1,17 +1,34 @@
 #!/usr/bin/perl -w
 @list = ();
+
+$url = "http://www.handbook.unsw.edu.au/undergraduate/courses/2015/$ARGV[0].html";
+open F, "wget -q -O- $url|" or die "can't open $url";
+
+while ($line = <F>) {
+	if ($line =~ "Prerequisite: .*<" || $line =~ "Prerequisites: .*<"){
+		$temp = $line;
+		$temp =~ s/<\/p>.*//;
+		chomp($temp);
+		push @list,($temp =~ m/[A-Z]{4}[0-9]{4}/g);
+
+	}
+}
+
 $url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2015/$ARGV[0].html";
 open F, "wget -q -O- $url|" or die "can't open $url";
 
 while ($line = <F>) {
-	if ($line =~ "Prerequisite: .*<"){
+	if ($line =~ "Prerequisite: .*<" || $line =~ "Prerequisites: .*<"){
 		$temp = $line;
-		#$temp =~ s/^(Prerequisite)//;
-		push @list, $temp;
+		$temp =~ s/<\/p>.*//;
+		chomp($temp);
+		push @list,($temp =~ m/[A-Z]{4}[0-9]{4}/g);
 	}
-    #print $line;
 }
+
+@list = sort @list;
+
 foreach $arg (@list){
-	print $arg;
+	print "$arg\n";
 }
 	
