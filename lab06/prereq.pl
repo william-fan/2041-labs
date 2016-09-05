1019d84142c5c2691cf3cf1232410cdd32e821b3
William Fan <william.fan1@student.unsw.edu.au>
2016-09-05 19:32:48 +1000

    passes autotests

--- lab06/prereq.pl
+++ lab06/prereq.pl
@@ -1,29 +1,22 @@
 #!/usr/bin/perl -w
 @list = ();
-
+$count = 0;
 $url = "http://www.handbook.unsw.edu.au/undergraduate/courses/2015/$ARGV[0].html";
 open F, "wget -q -O- $url|" or die "can't open $url";
 
-while ($line = <F>) {
-	if ($line =~ "Prerequisite: .*<" || $line =~ "Prerequisites: .*<"){
-		$temp = $line;
-		$temp =~ s/<\/p>.*//;
-		chomp($temp);
-		push @list,($temp =~ m/[A-Z]{4}[0-9]{4}/g);
 
+while ($count != 2){
+	while ($line = <F>) {
+		if ($line =~ "Prerequisite: .*<" || $line =~ "Prerequisites: .*<"){
+			$temp = $line;
+			$temp =~ s/<\/p>.*//;
+			chomp($temp);
+			push @list,($temp =~ m/[A-Z]{4}[0-9]{4}/g);
+		}
 	}
-}
-
-$url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2015/$ARGV[0].html";
-open F, "wget -q -O- $url|" or die "can't open $url";
-
-while ($line = <F>) {
-	if ($line =~ "Prerequisite: .*<" || $line =~ "Prerequisites: .*<"){
-		$temp = $line;
-		$temp =~ s/<\/p>.*//;
-		chomp($temp);
-		push @list,($temp =~ m/[A-Z]{4}[0-9]{4}/g);
-	}
+	$url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2015/$ARGV[0].html";
+	open F, "wget -q -O- $url|" or die "can't open $url";
+	$count++;
 }
 
 @list = sort @list;
