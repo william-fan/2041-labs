59e25244a8cbc338b28eedebce00387352f5c6f8
William Fan <william.fan1@student.unsw.edu.au>
2016-09-07 21:26:03 +1000

    passes autotests

--- lab07/total_words.pl
+++ lab07/total_words.pl
@@ -1,5 +1,16 @@
 #!/usr/bin/perl -w
+@words = ();
+$count = 0;
 
-while (<>){
-
+while ($lines = <STDIN>){	
+	chomp($lines);		
+	push @words , split(/([^A-Za-z])/, $lines); #split line on non chars
 }
+
+foreach $arg (@words){
+	chomp($arg);
+	if ($arg =~ "[A-Za-z]+" && $arg ne "" && $arg ne "\n"){ #if not empty or newline
+		$count++;		
+	}
+}
+print "$count words\n";	
