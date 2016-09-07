987878346d29531b0b41b26b666795aa6fe6b338
William Fan <gitlab@gitlab.cse.unsw.EDU.AU:z5059967/16s2-comp2041-labs.git>
2016-09-07 13:23:32 +1000

    passes autotests

--- lab06/whale.py
+++ lab06/whale.py
@@ -9,12 +9,12 @@ for line in sys.stdin:
 	total.append(line) #push input into array
 
 for arg in total:
-	arg.strip
+	arg.strip()
 	number, animal = arg.split(" ", 1) 		#split input into two
 
-	if sys.argv[1] == animal:
+	if sys.argv[1].strip() == animal.strip():
 		count += int(number) 	#add to count of individuals
 		pod += 1				#add to count of pod
 
 
-print ("%s observations: %s pods, %s individuals\n" % (sys.argv[1], pod, count))
+print ("%s observations: %s pods, %s individuals" % (sys.argv[1], pod, count))
