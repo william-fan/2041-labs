20b092542581932e04548816d4a713c5a42de95f
William Fan <william.fan1@student.unsw.edu.au>
2016-09-09 02:21:18 +1000

    remove needless loop

--- lab07/log_probability.pl
+++ lab07/log_probability.pl
@@ -14,18 +14,16 @@ foreach $file (glob "poems/*.txt") {
 		push @words , split(/([^A-Za-z])/, $lines); #split line on non chars
 	}
 	foreach $arg (@words){	#counting all words
+		$input = lc($ARGV[0]);
 		chomp($arg);
 		if ($arg =~ "[A-Za-z]+" && $arg ne "" && $arg ne "\n"){ #if not empty or newline
 			$count++;		
 		}
-	}	
-	foreach $arg (@words){	#counting specific word
-		$input = lc($ARGV[0]);
-		chomp($arg);
 		if ($arg =~ m/(^$input$)/g && $arg ne "" && $arg ne "\n"){ #if not empty or newline
 			$word++;	
 		}
-	}
+	}	
+
 	$table {"$file"}{"Count"} = $count;
 	$table {"$file"}{"Word count"} = $word;
 }
