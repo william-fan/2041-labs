7dfd2bea21706dc80f46d26cec9f96cbc734be3a
William Fan <william.fan1@student.unsw.edu.au>
2016-09-13 22:06:42 +1000

    first version

--- lab08/lectures0.pl
+++ lab08/lectures0.pl
@@ -1,15 +1,24 @@
 #!/usr/bin/perl -w
-#@list = ();
+
 foreach $arg (@ARGV){
+	%arg = ();
 	$url = "http://timetable.unsw.edu.au/current/$arg.html";
 	open F, "wget -q -O- $url|" or die "can't open $url";
 	while ($line = <F>){
-		if ($line =~ /[A-Z][a-z][a-z] [0-9]/){
+		if ($line =~ m/Lecture/){
+			for ($count = 0; $count!=6; $count++){
+				next;
+			}
 			$line =~ s/<\/td>//;
 			$line =~ s/<.*>//;
 			$line =~ s/\s+/ /g;
-			$arg{}
-			print "$line\n";
+			$arg{$line}="$line";
+
+			#print "$line\n";
+		
 		}
+	}
+	foreach $b (keys %arg){
+		print "$arg: $arg{$b}\n";
 	}
 }
