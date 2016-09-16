ed1b7d23246fa327a23cd09be2ef5fca8ddf0eee
William Fan <william.fan1@student.unsw.edu.au>
2016-09-16 17:40:17 +1000

    passes autotests

--- lab08/lectures0.pl
+++ lab08/lectures0.pl
@@ -1,24 +1,45 @@
 #!/usr/bin/perl -w
-
+@list = ();
 foreach $arg (@ARGV){
+	@list = ();
 	%arg = ();
 	$url = "http://timetable.unsw.edu.au/current/$arg.html";
 	open F, "wget -q -O- $url|" or die "can't open $url";
 	while ($line = <F>){
-		if ($line =~ m/Lecture/){
-			for ($count = 0; $count!=6; $count++){
-				next;
+		push @list, $line;
+	}
+	for ($count = 0; $count!=scalar @list; $count++){
+		if ($list[$count] =~ m/Lecture<\/a>/){
+			#print $list[$count];
+			$temp = $list[$count+6];
+			$sem = $list[$count+1];
+			chomp($temp);
+			chomp($sem);
+			$temp =~ s/<\/td>//;
+			$temp =~ s/<.*>//;
+			$temp =~ s/\s+/ /g;
+			$sem =~ s/<\/td>//;
+			$sem =~ s/<\/a>//;
+			$sem =~ s/<.*>//;
+			$sem =~ s/\s+/ /g;
+			$sem =~ tr/T/S/;
+			$sem =~ tr/U/X/;
+			#print $sem;
+			$bool = 0;
+			foreach $a (keys %arg){
+				if ($temp eq $arg{$a}){
+					$bool = 1;
+				}
 			}
-			$line =~ s/<\/td>//;
-			$line =~ s/<.*>//;
-			$line =~ s/\s+/ /g;
-			$arg{$line}="$line";
+			if ($temp =~ /[A-Z]/ && $bool == 0){
+				$arg{$count}="$temp";
+				$arg{$count}{sem}="$sem";
+				#print "$temp\n";
+			}
 
-			#print "$line\n";
-		
 		}
 	}
-	foreach $b (keys %arg){
-		print "$arg: $arg{$b}\n";
+	foreach $b (sort keys %arg){
+		print "$arg:$arg{$b}{sem}$arg{$b}\n";
 	}
 }
