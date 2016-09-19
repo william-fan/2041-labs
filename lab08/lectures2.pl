47e9fcc04e3bd63c75957ecc05ef68f3709e06bb
William Fan <william.fan1@student.unsw.edu.au>
2016-09-19 18:39:36 +1000

    mainly working

---
+++ lab08/lectures2.pl
@@ -0,0 +1,119 @@
+#!/usr/bin/perl -w
+@list = ();
+foreach $arg (@ARGV){
+	@list = ();
+	%arg = ();
+	$url = "http://timetable.unsw.edu.au/current/$arg.html";
+	open F, "wget -q -O- $url|" or die "can't open $url";
+	while ($line = <F>){
+		push @list, $line;
+	}
+	for ($count = 0; $count!=scalar @list; $count++){
+		if ($list[$count] =~ m/Lecture<\/a>/){
+			#print $list[$count];
+			$temp = $list[$count+6];
+			$sem = $list[$count+1];
+			chomp($temp);
+			chomp($sem);
+			$temp =~ s/<\/td>//;
+			$temp =~ s/<.*>//;
+			$temp =~ s/\s+/ /g;
+			$sem =~ s/<\/td>//;
+			$sem =~ s/<\/a>//;
+			$sem =~ s/<.*>//;
+			$sem =~ s/\s+/ /g;
+			$sem =~ tr/T/S/;
+			$sem =~ tr/U/X/;
+			#print $sem;
+			$bool = 0;
+			foreach $a (keys %arg){
+				if ($temp eq $arg{$a}){
+					$bool = 1;
+				}
+			}
+			if ($temp =~ /[A-Z]/ && $bool == 0){
+				$arg{$count}="$temp";
+				$arg{$count}{sem}="$sem";
+				#print "$temp\n";
+			}
+
+		}
+	}
+	foreach $b (sort keys %arg){
+		
+		@time = ();
+		if ($ARGV[0] eq "-d"){
+			push @time, split(" ", $arg{$b});
+			for ($temp=0; $temp != scalar @time; $temp++){
+				if ($time[$temp] =~ /[A-Za-z]{3}/){
+					$day = $time[$temp];
+					$day =~ /[A-Za-z]{3}/;
+					#print "$day\n";
+				}
+				if ($time[$temp] =~ /[0-9]{2}:[0-9]{2}/){
+					if (defined $time [$temp+2] && $time[$temp+2] =~ /[0-9]{2}:[0-9]{2}/){
+						$second = $time[$temp+2];
+						$second =~ s/:[0-9]{2}//;
+					}
+					$first = $time[$temp];
+					$first =~ s/:[0-9]{2}//;	
+					$s = $arg{$b}{sem};
+					$s =~ s/^\s+//;
+
+					while ($first != $second){
+						print "$s $arg $day $first\n";
+						$first++;
+					}
+				}
+			}
+		} elsif ($ARGV[0] eq "-t") {
+			push @time, split(" ", $arg{$b});
+			for ($temp=0; $temp != scalar @time; $temp++){
+				if ($time[$temp] =~ /[A-Za-z]{3}/){
+					$day = $time[$temp];
+					$day =~ /[A-Za-z]{3}/;
+					#print "$day\n";
+				}
+				if ($time[$temp] =~ /[0-9]{2}:[0-9]{2}/){
+					if (defined $time [$temp+2] && $time[$temp+2] =~ /[0-9]{2}:[0-9]{2}/){
+						$second = $time[$temp+2];
+						$second =~ s/:[0-9]{2}//;
+						$second =~ s/^0//;
+						#$second ++;
+						#print "$second|\n";
+					}
+					$first = $time[$temp];
+					$first =~ s/:[0-9]{2}//;
+					$first =~ s/^0//;	
+					$s = $arg{$b}{sem};
+					$s =~ s/^\s+//;
+					#print "$first\n";
+					while ($first != $second){
+						$session{$s}{$day}{$first}++;
+						$first++;
+					}
+				}
+			}
+			foreach my $semester (sort keys %session) { 
+
+				printf "%-6s",$semester;  
+				printf "%6s", $_ for qw(Mon Tue Wed Thu Fri);   
+				print "\n";
+				foreach $time (9..20) { 
+					printf "%02d:00", $time;
+					foreach $day (qw(Mon Tue Wed Thu Fri)) {
+						if($session{$semester}{$day}{$time}) {  
+							printf "%6s", $session{$semester}{$day}{$time};
+						} else {
+							printf "%6s","";
+						}
+					}
+					print "\n";
+				}
+			}
+
+		} else {
+			print "$arg:$arg{$b}{sem}$arg{$b}\n";
+		}
+	}
+}
